00d5ea55ecc86ce15239db8e12985e20bce095b5
Daniel Stoehr <daniel.stoehr@boschrexroth.de>
2022-04-06 14:47:50 +0200

    add skills

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { ButtonComponent } from './button/button.component';
 import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    HomeComponent,
-    ButtonComponent,
-    SkillsComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, SkillsComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.scss']
+  styleUrls: ['./skills.component.scss'],
 })
 export class SkillsComponent implements OnInit {
+  skills = [
+    { title: 'Javascript', img: 'assets/img/java script.png' },
+    { title: 'Angular', img: 'assets/img/angular.png' },
+    { title: 'HTML/CSS', img: 'assets/img/Group 205.png' },
+    { title: 'Git', img: 'assets/img/Git-Icon-1788C 2.png' },
+    { title: 'Rest API', img: 'assets/img/icons8-rest-api-80 2.png' },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
